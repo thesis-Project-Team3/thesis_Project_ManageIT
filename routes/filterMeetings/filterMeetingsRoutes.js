c2eda887a9cfb122ec2715993297e5f1fb5cb100
Adam-boulawdhen <Boulawdhenadam@gmail.com>
2020-07-29 05:02:04 -0700

    working on some topics using roles

--- routes/filterMeetings/filterMeetingsRoutes.js
+++ routes/filterMeetings/filterMeetingsRoutes.js
@@ -4,22 +4,23 @@ const Meeting = require("../../models/meetingSchema.js");
 
 router.post("/", (req, res) => {
   console.log(req.body.role);
-  if (req.body.role === "ceo") {
-    Meeting.find({}, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.send(err);
-      } else {
-        res.send(data);
-      }
-    });
-  } else if (req.body.role === "") {
-    Meeting.find({}, function (err, result) {});
-  } else if (req.body.role === "") {
-    Meeting.find({}, function (err, result) {});
-  } else if (req.body.role === "") {
-    Meeting.find({}, function (err, result) {});
-  }
+  // if (req.body.role === "ceo") {
+  Meeting.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+    // });
+  });
+  //  else if (req.body.role === "") {
+  //   Meeting.find({}, function (err, result) {});
+  // } else if (req.body.role === "") {
+  //   Meeting.find({}, function (err, result) {});
+  // } else if (req.body.role === "") {
+  //   Meeting.find({}, function (err, result) {});
+  // }
 });
 
 module.exports = router;
