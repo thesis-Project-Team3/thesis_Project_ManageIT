1f577f71bedbe48409e32bf132ba2b1ba6fb34e8
Oussema sferi <62106784+oussema-sferi@users.noreply.github.com>
2020-07-29 23:10:34 +0100

    projectinfohead fixed by getting info with props

--- client/src/views/ProjectHistoryHeads.js
+++ client/src/views/ProjectHistoryHeads.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
+import ProjectInfoHeads from './ProjectInfoHeads.js';
 // reactstrap components
 import {
   Card,
@@ -18,6 +19,8 @@ class ProjectHistoryHeads extends React.Component {
     super(props);
     this.state = {
       projects: [],
+      currentIndex: '',
+      view: 'false',
     };
   }
   componentDidMount() {
@@ -32,28 +35,33 @@ class ProjectHistoryHeads extends React.Component {
       });
   }
 
-  handleSubmit(i) {
-    // e.preventDefault();
-    axios
-      .post('http://localhost:5000/project/index', { index: i })
-      .then((response) => {
-        console.log(response.data);
-        window.location = '/admin/project-info-heads';
-      });
-    // .catch((err) => console.log('Error', err));
-  }
+  handleInfo = (id) => {
+    // console.log(id);
+    this.setState({ currentIndex: id, view: 'true' });
+  };
+
+  // handleSubmit(i) {
+  //   // e.preventDefault();
+  //   axios
+  //     .post('http://localhost:5000/project/index', { index: i })
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       window.location = '/admin/project-info-heads';
+  //     });
+  //   // .catch((err) => console.log('Error', err));
+  // }
 
   render() {
-    var ProjectHistory = this.state.projects.map((proj, i) => {
+    var ProjectHistory = this.state.projects.map((project) => {
       return (
-        <tr key={i}>
-          <td>{proj.title}</td>
-          <td>{proj.deadline.slice(0, 10)}</td>
-          <th>{proj.status}</th>
-          <th>{proj.progress}</th>
+        <tr key={project._id}>
+          <td>{project.title}</td>
+          <td>{project.deadline.slice(0, 10)}</td>
+          <th>{project.status}</th>
+          <th>{project.progress}</th>
           <td className="text-center">
             <Button
-              onClick={this.handleSubmit.bind(this, i)}
+              onClick={() => this.handleInfo(project._id)}
               color="link"
               id="buttonInfo"
               title=""
@@ -65,35 +73,40 @@ class ProjectHistoryHeads extends React.Component {
         </tr>
       );
     });
-    return (
-      <>
-        <div className="content">
-          <Row>
-            <Col md="12">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h4">Project List</CardTitle>
-                </CardHeader>
-                <CardBody>
-                  <Table className="tablesorter" responsive>
-                    <thead className="text-primary">
-                      <tr>
-                        <th>Title</th>
-                        <th>Do before</th>
-                        <th>Status</th>
-                        <th>Progress</th>
-                        <th className="text-center">Info</th>
-                      </tr>
-                    </thead>
-                    <tbody>{ProjectHistory}</tbody>
-                  </Table>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </>
-    );
+
+    if (this.state.view === 'false') {
+      return (
+        <>
+          <div className="content">
+            <Row>
+              <Col md="12">
+                <Card>
+                  <CardHeader>
+                    <CardTitle tag="h4">Project List</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <Table className="tablesorter" responsive>
+                      <thead className="text-primary">
+                        <tr>
+                          <th>Title</th>
+                          <th>Do before</th>
+                          <th>Status</th>
+                          <th>Progress</th>
+                          <th className="text-center">Info</th>
+                        </tr>
+                      </thead>
+                      <tbody>{ProjectHistory}</tbody>
+                    </Table>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </>
+      );
+    } else {
+      return <ProjectInfoHeads currentIndex={this.state.currentIndex} />;
+    }
   }
 }
 
